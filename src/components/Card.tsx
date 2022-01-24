36ced0ef96db7a1937fac87e50a41082035788d0
Sat Naing <satnaing@Sats-MacBook-Air.local>
2022-01-24 20:24:48 +0630

    :art: update Card for description and card url

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -2,20 +2,28 @@ import React from "react";
 import styles from "../styles/Card.module.css";
 import CardImg from "../public/web-listing-convenience-at-supermarket-prices.jpeg";
 import Image from "next/image";
+import Link from "next/link";
 
 type Props = {
   img: StaticImageData;
   title: string;
+  desc?: string;
+  url?: string;
 };
 
-const Card: React.FC<Props> = ({ img, title }) => {
+const Card: React.FC<Props> = ({ img, title, desc, url = "/" }) => {
   return (
-    <div className={styles.card}>
-      <Image src={img} alt={title} className={styles.image} />
-      <div className={styles.container}>
-        <h4>{title}</h4>
+    <Link href={url}>
+      <div className={`${styles.card} ${desc ? styles.left : styles.center}`}>
+        <Image src={img} alt={title} className={styles.image} />
+        <div className={styles.container}>
+          <h4 className={`${styles.title} ${desc && styles.smallMargin}`}>
+            {title}
+          </h4>
+          <p>{desc}</p>
+        </div>
       </div>
-    </div>
+    </Link>
   );
 };
 

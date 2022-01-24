b92f23882ab22c032095916e16407e1adb23c9a8
Sat Naing <satnaing@Sats-MacBook-Air.local>
2022-01-24 20:33:31 +0630

    :sparkles: implement pre-order page

---
+++ src/pages/pre-order.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { NextPage } from "next";
+
+// Custom
+import HeroStatic from "../components/HeroStatic";
+import NavBar from "../components/NavBar";
+import Section from "../components/Section";
+import Card from "../components/Card";
+import Footer from "../components/Footer";
+
+// Images
+import CardImg1 from "../public/web-listing-sunnyhills-pineapple-cake-sanrio-tote-bag-1880.png";
+
+import heroImg from "../public/sliders/web-banner-listing-2x-sunnyhills-pineapple-cake-sanrio-tote-bag-1880.jpeg";
+import heroImgMobile from "../public/sliders/resp-banner-listing-sunnyhills-pineapple-cake-sanrio-tote-bag-1880.jpeg";
+
+const cards = [
+  {
+    id: 1,
+    img: CardImg1,
+    title: "SUNNYHILLS PINEAPPLE CAKE & SANRIO TOTE BAG - $18.80",
+  },
+];
+
+const Offer: NextPage = () => {
+  return (
+    <div>
+      <NavBar />
+      <HeroStatic imgWeb={heroImg} imgMobile={heroImgMobile} />
+      <Section breadcrumb="Pre-order">
+        {cards.map((card) => (
+          <Card key={card.id} img={card.img} title={card.title} />
+        ))}
+      </Section>
+      <Footer />
+    </div>
+  );
+};
+
+export default Offer;

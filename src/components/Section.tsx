f62109bf527c0608d092f3535873407752769655
Sat Naing <satnaing@Sats-MacBook-Air.local>
2022-01-24 19:46:35 +0630

    :art: update Section for breadcrumb

--- src/components/Section.tsx
+++ src/components/Section.tsx
@@ -3,15 +3,32 @@ import React from "react";
 import styles from "../styles/Section.module.css";
 
 type Props = {
-  heading: string;
+  heading?: string;
   seeAllLink?: string;
+  breadcrumb?: string;
 };
 
-const Section: React.FC<Props> = ({ heading, seeAllLink, children }) => {
+const Section: React.FC<Props> = ({
+  heading,
+  seeAllLink,
+  breadcrumb,
+  children,
+}) => {
   return (
     <div className="row">
       <section className={styles.container}>
-        <h2 className={styles.heading}>{heading}</h2>
+        {breadcrumb ? (
+          <div className={styles.breadcrumb}>
+            <Link href="/">Home</Link>
+            <div className={styles.breadcrumbSeparator}> &rsaquo;</div>
+            <span>
+              <a className={styles.breadcrumbActive}>{breadcrumb}</a>
+            </span>
+          </div>
+        ) : (
+          <h2 className={styles.heading}>{heading}</h2>
+        )}
+
         {seeAllLink && (
           <div className={styles.seeAll}>
             <Link href={seeAllLink!}>See all</Link>
